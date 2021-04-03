4aabb7984d0ec9c3ce275843b4676bc8db878d6a
Herison Victor <72306935+herisonvictor@users.noreply.github.com>
2021-04-03 10:44:45 -0300

    1008 - Salário

---
+++ Iniciante/URI_1008.py
@@ -0,0 +1,6 @@
+número_funcionário = int(input())
+horas_trabalhadas = int(input())
+horas = float(input())
+salário = horas_trabalhadas * horas
+print(f'NUMBER = {número_funcionário}')
+print(f'SALARY = U$ {salário:.2f}')
